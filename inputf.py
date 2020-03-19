354bbf6d41410b535447d5c26280351e0b799c91
weimengbao <u8tou@qq.com>
2020-03-20 00:28:39 +0800

    This is my learn Python test.

---
+++ inputf.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# @Author  : wei mengbao
+# @Email   : u8tou@qq.com
+# @project : robot
+# @Time    : 2020/2/29 18:20
+def infto():
+    print('这是一个测试类')
+if __name__ == '__main__':
+    infto()
